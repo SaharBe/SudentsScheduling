4eed8cdf51002001c5e41e46598099e5925601c9
SAHAR BEN YITZHAK <sahar.benyitzhak@live.biu.ac.il>
2021-11-02 14:06:51 +0200

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+
+    boolean englishLeadership;
+    enum behavior {A, B, C};
+    enum grades {A,B, C};
+    Student[] friends;
+    Student[] beWith;
+    Student[] notBeWith;
+    int classroom;
+
+}
