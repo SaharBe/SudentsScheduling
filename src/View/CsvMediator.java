f16f047d52a4e641290530ecbe48d96015477819
IdoAvTr <81075460+IdoAvTr@users.noreply.github.com>
2021-12-22 13:05:23 +0200

    added method Return Output in the csv mediator

--- src/View/CsvMediator.java
+++ src/View/CsvMediator.java
@@ -41,6 +41,7 @@ public class CsvMediator implements DataMediator {
     }
     // may wanna return the path to the file
     public String ReturnOutput(List<List<String>> classes) throws IOException {
+        // WHERE THE FILE SHOULD BE LOCATED IN?
         String address = "";
         File file = new File(address);
         // create FileWriter object with file as parameter
@@ -48,7 +49,7 @@ public class CsvMediator implements DataMediator {
             for (List<String> list:classes) {
                 String line = convertToCSV(list.toArray(new String[0]));
                 if (line == null)
-                        return null;        //or check the prob
+                        return null;        //IS THERE A VALID CASE FOR RETURNING NULL?
                 writer.write(line);
             }
         }
