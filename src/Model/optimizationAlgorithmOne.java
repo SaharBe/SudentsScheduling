1248af84838f74a28de35a68598175bc76abd614
SaharBe <57907970+SaharBe@users.noreply.github.com>
2021-12-31 03:42:03 +0200

    Merge branch 'master' of C:\Users\Sahar\Documents\GitHub\SudentsScheduling with conflicts.

---
+++ src/Model/optimizationAlgorithmOne.java
@@ -0,0 +1,12 @@
+package Model;
+import com.google.ortools.Loader;
+import com.google.ortools.linearsolver.MPConstraint;
+import com.google.ortools.linearsolver.MPObjective;
+import com.google.ortools.linearsolver.MPSolver;
+import com.google.ortools.linearsolver.MPVariable;
+
+
+public class optimizationAlgorithmOne implements OptimizationAlgorithm {
+g
+
+}
