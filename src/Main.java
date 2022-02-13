8812ea156db1fa9e480cdf9f59e9cd7dc2748206
Sahar Ben Yitzhak <saharby1996@gmail.com>
2022-02-13 18:37:19 +0200

    some changes

--- src/Main.java
+++ src/Main.java
@@ -7,55 +7,83 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        //TheController controller = new TheController();
+       // TheController controller = new TheController();
         //controller.StartProgram();
-        CSVMediatorTester csvMediatorTester = new CSVMediatorTester();
-        csvMediatorTester.Test();
+       // CSVMediatorTester csvMediatorTester = new CSVMediatorTester();
+       // csvMediatorTester.Test();
+    //    System.out.println("test" );
 
 
 
-//        ArrayList<Student> s = new ArrayList<Student>(5);
-//        Student alis = new Student(1,"alis","f",false,false,Level.LOW, Level.MEDIUM,null,null,null);
+
+          ArrayList<Student> s = new ArrayList<Student>(5);
+        Student alis = new Student(1,"alis","f",false,false,Level.LOW, Level.MEDIUM,null,null,null);
+
+        Student[] bobFriends = {alis};
+        //bobFriends = new ArrayList<>(3);
+        Student bob = new Student(2,"bob","m", true,true,Level.HIGH, Level.MEDIUM, bobFriends,null,null );
+
+        Student[] cFriends = {alis, bob};
+
+
+        Student coco = new Student(3,"coco","m",true,true,Level.HIGH, Level.LOW, cFriends, null,null);
+
+        Student davis = new Student(4, "davis","m", true,false,Level.LOW, Level.LOW, cFriends, null, null);
+
+        Student helena = new Student(5, "helena","f", false,false,Level.MEDIUM, Level.MEDIUM, null, null, null);
+
+        Student emma = new Student(6, "emma","f", false,false,Level.MEDIUM, Level.HIGH, null, null, null);
+
+        Student mia = new Student(7, "mia","f", true,false,Level.LOW, Level.HIGH, null, null, null);
+
+        Student zoe = new Student(8, "zoe","f", false,false,Level.HIGH, Level.LOW, null, null, null);
+
+        Student f = new Student(9, "f","m", false,false,Level.HIGH, Level.HIGH, null, null, null);
+
+        Student g = new Student(10, "g","m", false,false,Level.MEDIUM, Level.MEDIUM, null, null, null);
+
+        Student h = new Student(11, "h","m", false,false,Level.HIGH, Level.MEDIUM, null, null, null);
+
+        Student k = new Student(12, "k","f", false,true,Level.HIGH, Level.MEDIUM, null, null, null);
+
+
+        Class one = new Class(1,3,5);
+        Class two = new Class(2,3,5);
+        Class three =  new Class(3,3,5);
+
+        List<Student> students = new ArrayList<>();
+        students.add(alis);
+        students.add(bob);
+        students.add(coco);
+        students.add(davis);
+        students.add(helena);
+        students.add(emma);
+        students.add(mia);
+        students.add(zoe);
+        students.add(f);
+        students.add(g);
+        students.add(h);
+        students.add(k);
+
+
+
+
+        List<Class> classes = new ArrayList<>();
+        classes.add(one);
+        classes.add(two);
+        classes.add(three);
+
+        OptimizationAlgo opt = new OptimizationAlgo(students, classes);
+//        for (int i = 0; i < 3; i++) {
+//            System.out.println(opt.getNumOfBehavior()[i]);
+//            System.out.println(opt.getNumOfGrades()[i]);
+//        }
 //
-//        Student[] bFriend = {alis};
-//        //bFriend = new ArrayList<>(3);
-//
-//        Student bob = new Student(2,"bob","m", true,true,Level.HIGH, Level.MEDIUM, bFriend,null,null );
-//
-//        Student[] cFriends = {alis, bob};
-//
-//
-//        Student coco = new Student(3,"coco","m",true,true,Level.HIGH, Level.LOW, cFriends, null,null);
-//
-//        Student davis = new Student(4, "davis","m", true,false,Level.LOW, Level.LOW, cFriends, null, null);
-//
-//        Class one = new Class(1,3,5);
-//
-//        List<Student> students = new ArrayList<>();
-//        students.add(alis);
-//        students.add(bob);
-//        students.add(coco);
-//        students.add(davis);
-//        List<Class> classes = new ArrayList<>();
-//        classes.add(one);
+        System.out.println(opt.getNumOfGrades());
+        System.out.println(opt.getNumOfBehaviors());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println("gg"+classes.size());
+        opt.averageClassData();
 
 
 
--- src/Model/OptimizationAlgo.java
+++ src/Model/OptimizationAlgo.java
@@ -1,34 +1,151 @@
 package Model;
+import java.util.HashMap;
 import java.util.List;
+import java.util.*;
 
 public class OptimizationAlgo implements OptimizationAlgorithm {
 
-//    private List<Student> students;
-//    private List<Class> classes;
-//
-//
-//    public OptimizationAlgo(List<Student> students, List<Class> classes){
-//        this.students = students;
-//        this.classes = classes;
-//    }
-//
-//    public int getNumOfStudents(){
-//        return students.size();
-//    }
-//
-//    public int getNumOfBoys(){
-//        int boysCounter = 0;
-//        for(Student s: students){
-//            if (s.getGender().equals("m") || s.getGender().equals("M")){
-//                boysCounter++;
-//            }
-//        }
-//        return boysCounter;
-//    }
-//
-//    public int getNumOfGirls(){
-//        return (getNumOfStudents() - getNumOfBoys());
-//    }
+    private List<Student> students;
+    private List<Class> classes;
+
+
+    public OptimizationAlgo(List<Student> students, List<Class> classes){
+        this.students = students;
+        this.classes = classes;
+    }
+
+    public int getNumOfStudents(){
+        return students.size();
+    }
+
+    public int getNumOfBoys(){
+        int boysCounter = 0;
+        for(Student s: students){
+            if (s.getGender().equals("m") || s.getGender().equals("M")){
+                boysCounter++;
+            }
+        }
+        return boysCounter;
+    }
+
+    public int getNumOfGirls(){
+        return (getNumOfStudents() - getNumOfBoys());
+    }
+
+    public int[] getNumOfBehavior(){
+        int HighCounter = 0;
+        int MediumCounter = 0;
+        int LowCounter = 0;
+
+        for(Student s: students){
+            if (s.getBehavior() == Level.HIGH){
+                HighCounter++;
+            }
+            if(s.getBehavior() == Level.MEDIUM){
+                MediumCounter++;
+            }
+            if(s.getBehavior() == Level.LOW){
+                LowCounter++;
+            }
+        }
+        int sum[] = { HighCounter, MediumCounter,LowCounter };
+
+        return sum;
+
+    }
+
+    public HashMap<String,Integer> getNumOfGrades(){
+        int HighCounter = 0;
+        int MediumCounter = 0;
+        int LowCounter = 0;
+
+        for(Student s: students){
+            if (s.getGrades() == Level.HIGH){
+                HighCounter++;
+            }
+            if(s.getGrades() == Level.MEDIUM){
+                MediumCounter++;
+            }
+            if(s.getGrades() == Level.LOW){
+                LowCounter++;
+            }
+        }
+
+        HashMap<String,Integer> sums =new HashMap<String, Integer>();
+        sums.put("HIGH", HighCounter);
+        sums.put("MEDIUM", MediumCounter);
+        sums.put("LOW", LowCounter);
+
+        return sums;
+    }
+
+    public HashMap<String,Integer> getNumOfBehaviors(){
+        int HighCounter = 0;
+        int MediumCounter = 0;
+        int LowCounter = 0;
+
+        for(Student s: students){
+            if (s.getBehavior() == Level.HIGH){
+                HighCounter++;
+            }
+            if(s.getBehavior() == Level.MEDIUM){
+                MediumCounter++;
+            }
+            if(s.getBehavior() == Level.LOW){
+                LowCounter++;
+            }
+        }
+
+        HashMap<String,Integer> sums =new HashMap<String, Integer>();
+        sums.put("HIGH", HighCounter);
+        sums.put("MEDIUM", MediumCounter);
+        sums.put("LOW", LowCounter);
+
+        return sums;
+    }
+
+    /*have to return:
+    * average a b c behavior
+    * average a b c grades
+    * average boy amount
+    * average girls amount*/
+    public void averageClassData() {
+
+        HashMap<String, Integer> allAverage = new HashMap<String, Integer>();
+        int numOfClasses = classes.size();
+
+        int aBehavior;
+        int bBehavior;
+        int cBehavior;
+        int aGrades;
+        int bGrades;
+        int cGrades;
+        int boysNum;
+        int girlsNum;
+
+        HashMap<String, Integer> grades = getNumOfGrades();
+//        Iterator gradesIterator = grades.entrySet().iterator();
+
+        // Iterating through Hashmap
+        // Getting a Set of Key-value pairs
+        Set entrySet = grades.entrySet();
+
+        // Obtaining an iterator for the entry set
+        Iterator it = entrySet.iterator();
+
+        // Iterate through HashMap entries(Key-Value pairs)
+        System.out.println("HashMap Key-Value Pairs : ");
+        while (it.hasNext()) {
+            Map.Entry me = (Map.Entry) it.next();
+            System.out.println(" Grade Key is: " + me.getKey() + " & " + " av value for evert class: " + me.getValue());
+
+
+        }
+    }
+
+
+
+
 
 
 
