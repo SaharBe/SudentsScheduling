447579d5249e1b007204e3143bec2286a158f214
IdoAvTr <81075460+IdoAvTr@users.noreply.github.com>
2022-03-01 13:02:55 +0200

    dataToDB inside BasicParser done.

--- src/Model/BasicParser.java
+++ src/Model/BasicParser.java
@@ -3,16 +3,36 @@ package Model;
 import java.util.List;
 
 public class BasicParser implements Parser {
+    Person prototype;
+    List<String> attributes;
+    boolean isGender = false;
 
-    //leaf parser will receive the student from the basic parser and will add the advanced attributes.
-    protected Student createStudent(List<String> values){
-        return new Student(Integer.parseInt(values.get(0)), values.get(1), values.get(2));
+    public BasicParser(Person object, List<String> attrs){
+        prototype = object;
+        attributes = attrs;
+        if (attributes.get(2).toLowerCase().equals("gender"))
+            isGender = true;
     }
+
+    //leaf parser will receive the person from the basic parser and will add the specific attributes.
+    protected Person createPerson(List<String> values){
+        boolean gender = true;
+        if (isGender)
+            gender = Boolean.parseBoolean(values.get(2));
+        Person person =  prototype.create(Integer.parseInt(values.get(0)), values.get(1), gender);
+        int i = isGender ? 3 : 2, n = attributes.size();
+        for (; i < n; i++){
+            person.setAttribute(attributes.get(i), values.get(i));
+        }
+        return person;
+    }
+
     @Override
     public DataBase dataToDB(List<List<String>> data) {
         DataBase db = new DataBase();
-        for(int i = 0; i < data.size(); i++){
-            createStudent(data.get(i));     //add to DB
+        for (List<String> datum : data) {
+            Person person = createPerson(datum);     //add to DB
+            db.add(person);
         }
         return null;
     }
--- src/Model/DataBase.java
+++ src/Model/DataBase.java
@@ -2,5 +2,9 @@ package Model;
 
 public class DataBase {
     // Map<int, Student>
+    public void add(Person person){}
     // owning all the Students and do operations on them (iterating for now)
+
+
+    // need to implement iterators
 }
