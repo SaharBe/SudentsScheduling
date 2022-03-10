8348523751d83c4c8a16ccb0f15f71c8d897d0b4
IdoAvTr <81075460+IdoAvTr@users.noreply.github.com>
2022-03-10 22:11:14 +0200

    DB almost finished need to implement the iterator

--- src/Model/DataBase.java
+++ src/Model/DataBase.java
@@ -3,11 +3,12 @@ package Model;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Set;
 
 public class DataBase {
     // store all peoples by ID
     List<String> attributes;
-    private Hashtable<Integer, Person> peoplesById = new Hashtable();
+    private Hashtable<Integer, Student> studentsById = new Hashtable();
     private Hashtable<String, Integer> typesCount = new Hashtable<>();
     private Hashtable<String, List<Integer>> typesList = new Hashtable<>();
     private Hashtable<String, Integer> offsets = new Hashtable<>();
@@ -15,7 +16,7 @@ public class DataBase {
 
 
     public void addStudent(Student student){
-        peoplesById.put(student.getID(), student);
+        studentsById.put(student.getID(), student);
         String key = student.generateKey();
         if (typesCount.get(key) == null){
             typesCount.put(key, 0);
@@ -25,11 +26,18 @@ public class DataBase {
         typesCount.put(key, typesCount.get(key) + 1);
         typesList.get(key).add(student.getID());
     }
-    // owning all the Students and do operations on them (iterating for now)
-    public Student getStudent(int id){ return null; }
-    public int amountsOfTypes(String type){return 1;}
-    public String[] getTypes(){ return null;}
-    // need to implement iterators
+    // get student by id
+    public Student getStudent(int id){ return studentsById.get(id); }
+    // get the amount of each type of student.
+    public int amountsOfTypes(String type){return typesCount.get(type);}
+    //get list of the types
+    public Set<String> getTypes(){ return typesCount.keySet();}
+    /**
+     * iterate through students according to their type
+     * @param type the type of the student (null is valid and represent iterating through all students)
+     * @return  the next student of this type
+     */
+    //the suggested way is to use 'offsets' as indicators of the next student of each type
     public Student next(String type){ return null;}
     public boolean hasNext(String type) {return true;}
 }
